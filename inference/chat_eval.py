9706d3c87d44da037fa1950afa857c99e628776e
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-02-15 16:06:44 -0800

    feat: new prompt and evaluation updates

--- inference/chat_eval.py
+++ inference/chat_eval.py
@@ -9,12 +9,9 @@ system_prompt = {
     "role":"system",
     "content":"""Determine the relationship between the `Premise`and `Hypothesis` and respond with an answer. 
 You must respond with an answer of `Entailment`, `Neutral` or `Contradiction`
-You need to respond in the format shown in the following:
-```json
-{"my_answer":<your answer>}
-```
-where you replace <your answer> with either `Entailment`, `Neutral` or `Contradiction`.
-You are allowed to lay out steps to reach a final answer, but you must respond in this format or else your answer will be rejected."""
+You need to respond in the format shown in the following by chosing one of thosw answers:
+<my_answer>[place your answer here]</my_answer>
+You are lay out the steps to your final answer before responding with your final answer, but you must respond in this format or else your answer will be rejected."""
 }
 
 def extract_classification(response_chat):
@@ -22,8 +19,31 @@ def extract_classification(response_chat):
     assert(assistant_response["role"] == "assistant")
     content = assistant_response.get("content", "")
     print(f"\n\033[92mAssistant's Response:\033[0m {content}")
-    raw_json_answer = re.findall("```json\n(.*)\n```",content,re.DOTALL)[0]
-    return json.loads(raw_json_answer).get("my_answer",None)
+    raw_json_answer = re.findall("<my_answer>(.*)</my_answer>",content,re.DOTALL)
+    accepetd_answers = set([ "entailment", "neutral", "contradiction"])
+    if len(raw_json_answer) > 0:
+        answer = raw_json_answer[0].lower()
+        if answer in accepetd_answers:
+            return answer
+    else:
+        return None
+
+def extract_first(response_chat):
+    assistant_response = response_chat[2]
+    assert(assistant_response["role"] == "assistant")
+
+    content = assistant_response.get("content", "").lower()
+
+    print(f"\n\033[92mAssistant's Response:\033[0m {content}")
+
+    classifications = [ "entailment", "neutral", "contradiction"]
+    classifications = {k: content.find(k) for k in classifications}
+
+    return min(
+        filter(lambda x : x[1] >= 0, classifications.items()),
+        key=lambda kv : kv[1],
+        default=(None,None) 
+    )[0]
 
 def build_NLI_prompt(example):
     test_example = f"Premise: {example["premise"]}\nHypothesis: {example["hypothesis"]}"
@@ -37,7 +57,7 @@ tokenizer.padding_side = "left" #for batched prompts so tokens are of the form [
 
 pipe = pipeline("text-generation", model=model,tokenizer=tokenizer)
 dataset = load_dataset("snli", split="validation")
-SNLI_query = dataset.map(build_NLI_prompt)[:]
+SNLI_query = dataset.map(build_NLI_prompt)[:1024]
 
 
 # lables, Y
@@ -49,11 +69,11 @@ word_labels = [classification_map[i] for i in labels_raw]
 reduced_prompts = SNLI_query["prompt"]
 
 print("Starting inference.")
-responses_raw = pipe(reduced_prompts,max_new_tokens=10_000,batch_size=100)
+responses_raw = pipe(reduced_prompts,max_new_tokens=10_000,batch_size=64,num_workers=8)
 print("Inference finished!")
 
 responses = [resp[0]["generated_text"] for resp in responses_raw]
-predictions = [extract_classification(resp) for resp in responses]
+predictions = [extract_first(resp) for resp in responses]
 
 def NLI_statsitics(x,y):
     if x == y:
